30e03359b007645d59d290448071fd0fbed5f2f3
Abhishek Singh <81193958+asingh2023@users.noreply.github.com>
2023-05-16 17:25:13 -0400

    Enhancing config validator

--- scripts/config-validator.js
+++ scripts/config-validator.js
@@ -1,87 +1,116 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
-const yaml = require('js-yaml');
-const SwaggerParser = require('@apidevtools/swagger-parser'); 
+const yaml = require('js-yaml'); 
 const args = process.argv.slice(2); 
 const folder = args?.[0]+"/config"; 
-const {errorMessage  , printMessage} = require('./utils/tools')
-const ded_validator  = 'DED VALIDATOR'; 
+const {errorMessage , errorMsg  , printMessage} = require('./utils/tools')
+const ded_validator  = 'DED VALIDATOR';
+const pdl_validator  = 'Product Layout VALIDATOR'; 
 const tenant_config_validator = 'TENANT CONFIG VALIDATOR'; 
 let check = true;
-const validateDir = async (dir) => {
 
+const validateDir = async (dir) => { 
 
-  fs.readdir(dir, { withFileTypes: true }, (err, files) => {
-    files?.forEach(async file => {
+  const files = await fs.promises.readdir(dir, { withFileTypes: true });
+  
+  for (const file of files) {
+    let check = true;
+    
+    if (file.name === 'document-explorer-definition.yaml'){ 
+      try {
+        const fileName = `${dir}/${file.name}`;
+        const content = await fs.promises.readFile(fileName, 'utf8');
+        const apiJson = yaml.load(content);  
+      } catch (e) {
+        errorMessage(ded_validator  ,e?.message);
+        check = false;
+      }
+      if (check){
+        printMessage(`${ded_validator} : PASSED`); 
+      }else{
+        errorMessage(ded_validator  ,"FAILED");
+      }
+    } 
 
-     if (file.name === 'document-explorer-definition.yaml'){ 
-        try {
-          const fileName = `${dir}/${file.name}`;
-          const content = fs.readFileSync(fileName, 'utf8');
-          const apiJson = yaml.load(content);  
-        } catch (e) {
-          errorMessage(ded_validator  ,e?.message);
-          check = false;
-        }
+    if (file.name === 'product-layout.yaml'){ 
+      try {
+        const fileName = `${dir}/${file.name}`;
+        const content = await fs.promises.readFile(fileName, 'utf8');
+        const apiJson = yaml.load(content);  
+      } catch (e) {
+        errorMessage(pdl_validator  ,e?.message);
+        check = false;
+      }
+      if (check){ 
+        printMessage(`${pdl_validator} : PASSED`); 
+      }else{
+        errorMessage(pdl_validator  ,"FAILED");
+      }
+    } 
+
+    if (file.name === 'tenant.json'){
+      try{ 
+        const fileName = `${dir}/${file.name}`;
+        const content = await fs.promises.readFile(fileName, 'utf8'); 
+        const data = JSON.parse(content); 
+        check = validateSpecExistence(args?.[0] , data); 
+      }catch (e){
+        errorMessage(tenant_config_validator  ,e?.message);
+        check = false;
       } 
 
-      if (file.name === 'tenant.json'){
-
-        try{ 
-          const fileName = `${dir}/${file.name}`;
-          const content = fs.readFileSync(fileName, 'utf8'); 
-          JSON.parse(content)
-          
-        }catch (e){
-          errorMessage(tenant_config_validator  ,e?.message);
-          check = false;
-        } 
+      if (check){ 
+        printMessage(`${tenant_config_validator} : PASSED`);
+      }else{
+        errorMessage( tenant_config_validator , 'FAILED');
       }
-      
-    });
-
-    if (check){
-      printMessage(`${tenant_config_validator} and ${ded_validator}: PASSED`);
-      }
+    } 
+  }
+}; 
+  
+const validateSpecExistence = (dir , tenantData)=> {  
+  let specExistence = true;
+  
+  if (tenantData?.apiVersions.length > 0){
+    for (const item of tenantData.apiVersions) {  
+      const version = item.version ;  
+      const apiSpecFiles = item.apiSpecFileNames ; 
+        
+      if (apiSpecFiles.length > 0){
+        for (const filePath of apiSpecFiles) {
+          const file = `${dir}/reference/${version}/${filePath}.yaml`;
+          if (!fs.existsSync(file)) {  
+            errorMsg(`${file} - Missing`);
+            specExistence = false;
+          }
+        }
+      }  
+    }
+  } 
     
-  });
+  return specExistence;
 };
 
-// function linkcheck(jsonArray){
 
-//     jsonArray.forEach((item) => { 
-//         for (res of item?.sections){  
-            
-//             if (res instanceof Object){
-//               //console.log(res);
-//                 if (res?.link){
-//                   console.log(res?.link);
-//                 }
-//                 for (res1 of res?.sections){
-//                   console.log(res1?.link);
-//                   if (res1?.sections instanceof Object){
-//                     linkcheck(res1?.sections)
-//                   }
-                 
-//                 }
-//             }
-          
-//         } 
-//       }); 
-//     return true;
-// }
-
-try {
-  printMessage(`External Dir ---->>> ${folder}`);   
-  if ( args?.length > 0){ 
-  validateDir(folder);
-  if (check){
-    printMessage(`External Dir ---->>> ${folder}`); 
+const main = async() => { 
+  try {
+    printMessage(`External Dir ---->>> ${folder}`);   
+    if ( args?.length > 0){ 
+      await validateDir(folder);
+      if (check){
+        printMessage(`External Dir ---->>> ${folder}`); 
+      }
+    }else{
+      errorMessage('YAML VALIDATOR'  ,'No Path for reference dir. defined');
+    }
+  } catch (e) {
+    errorMessage('YAML VALIDATOR'  ,e.message);
   }
- }else{
-  errorMessage('YAML VALIDATOR'  ,'No Path for reference dir. defined');
- }
-} catch (e) {
-  errorMessage('YAML VALIDATOR'  ,e.message);
+
+};
+
+if (require.main === module) {
+  main();
 }
+
